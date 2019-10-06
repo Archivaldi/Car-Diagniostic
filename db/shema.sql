5f3be4890cabea5d32354b78d3036022c523e96c
Artur Markov <artur.markov1860@gmail.com>
2019-10-06 12:03:30 -0700

    login and signup

--- db/shema.sql
+++ db/shema.sql
@@ -4,30 +4,19 @@ USE car_diagnostic;
 
 CREATE TABLE users
 (
-    user_id INT NOT NULL
-    AUTO_INCREMENT,
-    user_name VARCHAR
-    (255),
-    user_p_hash VARCHAR
-    (255),
-PRIMARY KEY
-    (user_id)
+    user_id INT NOT NULL AUTO_INCREMENT,
+    user_email VARCHAR(255),
+    user_name VARCHAR (255),
+    user_p_hash VARCHAR (255),
+    PRIMARY KEY (user_id)
 );
 
-    CREATE TABLE car_data
-    (
-        car_id INT NOT NULL
-        AUTO_INCREMENT,
-        user_id INT NOT NULL
-        car_name VARCHAR
-        (255),
-     car_model VARCHAR
-        (255),
-        car_make VARCHAR
-        (255),
-     car_year INT NOT NULL
-     PRIMARY KEY
-        (car_id),
-        FOREIGN KEY user_id REFERENCES users
-        (user_id)
+    CREATE TABLE car_data(
+        car_id INT NOT NULL AUTO_INCREMENT,
+        user_id INT NOT NULL,
+        car_model VARCHAR (255),
+        car_make VARCHAR (255),
+        car_year INT NOT NULL,
+        PRIMARY KEY (car_id),
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
  );
--- server.js
+++ server.js
@@ -8,6 +8,17 @@ const mysql = require("mysql");
 
 const path = require("path");
 
+// //session stuff
+var cookieParser = require('cookie-parser');
+var bcrypt = require('bcryptjs');
+
+var session = require('express-session');
+app.use(cookieParser());
+
+//allow sessions
+app.use(session({ secret: 'app', cookie: { maxAge: 1 * 1000 * 60 * 60 * 24 * 365 } }));
+
+
 //add dotenv package for hiding private data
 require("dotenv").config();
 const keys = require("./keys.js");
@@ -25,8 +36,64 @@ connection.connect(function (err) {
     console.log("Database connected");
 });
 
-app.get("/OBD_LookUp", function(req,res){
+app.get("/OBD_LookUp", function (req, res) {
     res.render("obdlookup");
+});
+
+app.get("/sign-up", function (req,res){
+    res.render("signup");
+})
+
+app.get("/log-in", function (req,res){
+    res.render("login");
+})
+app.post("/login", function (req, res) {
+    var userEmail = req.body.email;
+    var password = req.body.password;
+
+    connection.query('SELECT * FROM users LEFT JOIN car_data USING (user_id) WHERE user_email = ?', [userEmail], function (error, results, fields) {
+        if (error) throw error;
+        if (results == 0) {
+            res.send("Invalid Email and/or password. Please try again");
+        } else {
+            bcrypt.compare(password, results[0].user_p_hash, function (err, result) {
+
+                if (result == true) {
+
+                    req.session.user_id = results[0].user_id;
+                    req.session.email = results[0].user_email;
+                    req.session.name = results[0].user_name;                    
+                    req.session.car_model = results[0].car_model;                    
+                    req.session.car_make = results[0].car_make;                    
+                    req.session.car_year = results[0].car_year;                                      
+                    res.send('you are logged in');
+                    //res.redirect(profile page will go here);  
+
+
+                } else {
+                    res.redirect('/');
+                }
+            });
+        }
+    });
+});
+
+app.get('/another-page', function(req, res){
+	var user_info = {
+		user_id : req.session.user_id,
+        email: req.session.email,  
+        name: req.session.name,                   
+        car_model: req.session.car_model,                  
+        car_make: req.session.car_make,                   
+        car_year: req.session.car_year   
+    }
+    res.json(user_info);
+});
+
+app.get('/logout', function(req, res){
+	req.session.destroy(function(err){
+		res.send('you are logged out');
+	})
 });
 
 
@@ -35,10 +102,91 @@ app.get("/", function (req, res) {
         res.render("index", { res: result });
     })
 })
-app.get("/signup", function (req, res) {
-    connecttion.query
+
+
+
+app.post("/signup", function (req, res) {
+    var userEmail = req.body.email;
+    var userName = req.body.name;
+    var password = req.body.password;
+    var carMake = req.body.carmake;
+    var carModel = req.body.carModel;
+    var carYear = req.body.year;
+
+    function selectNewUserId(email) {
+        connection.query("SELECT * FROM users WHERE user_email=?", [email], function (err, result) {
+            insertCar(result[0].user_id, carMake, carModel, carYear);
+        })
+    }
+
+
+    function insertCar(userId, make, model, year) {
+        connection.query("INSERT INTO car_data (user_id, car_model, car_make, car_year) VALUES (?, ?, ?, ?)", [userId, make, model, year], function (err, result) {
+            res.send("You signed up!")
+            //res.redirect(profile page will go here);  
+        })
+    }
+
+    bcrypt.genSalt(10, function (err, salt) {
+        console.log(salt);
+        bcrypt.hash(password, salt, function (err, p_hash) {
+            console.log(p_hash);
+            connection.query("INSERT INTO users(user_email, user_name, user_p_hash) VALUES (?, ?, ?)", [userEmail, userName, p_hash], function (err, result) {
+                if (err) {
+                    console.log(err);
+                    res.send("You need to use a unique email")
+                } else {
+                    selectNewUserId(userEmail);
+                }
+            })
+        })
+    })
 })
 
 app.listen(3000, function () {
     console.log("Listening on 3000");
 });
+
+
+
+// // localhost:3000/login/haven@yahoo.com/goldenwarriors24
+// app.get('/login/:email/:password', function(req, res){
+
+// 	connection.query('SELECT * FROM users WHERE email = ?', [req.params.email],function (error, results, fields) {
+
+// 	  if (error) throw error;
+
+// 	  // res.json(results);
+
+// 	  if (results.length == 0){
+// 	  	res.send('try again');
+// 	  }else {
+// 	  	bcrypt.compare(req.params.password, results[0].password_hash, function(err, result) {
+
+// 	  	    if (result == true){
+
+// 	  	      req.session.user_id = results[0].id;
+// 	  	      req.session.email = results[0].email;
+
+// 	  	      res.send('you are logged in');
+
+// 	  	    }else{
+// 	  	      res.redirect('/');
+// 	  	    }
+// 	  	});
+// 	  }
+// 	});
+// });
+
+// app.get('/another-page', function(req, res){
+// 	var user_info = {
+// 		user_id : req.session.user_id,
+// 		email: req.session.email
+// 	}
+
+// 	res.json(user_info);
+// });
+
+// app.listen(3000, function(){
+// 	console.log('listening on 3000');
+// });
