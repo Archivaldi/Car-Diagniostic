2004b73c2540ab8a96905dc2975435208405a0e7
Irina K <irtiola@gmail.com>
2019-10-06 19:09:03 -0700

    new info

--- keys.js
+++ keys.js
@@ -1,7 +1,6 @@
 exports.data = {
-    host: process.env.LOCAL_HOST_NAME,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-  };
-  
+  host: process.env.LOCAL_HOST_NAME,
+  user: process.env.USER_NAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
+};
