a5cc19c94ed6dc64e16a23288d67b1825ad4c142
Irtiola <irtiola@gmail.com>
2019-10-08 12:51:32 -0700

    only added bootstrap script

--- server.js
+++ server.js
@@ -23,46 +23,47 @@ app.use(session({ secret: 'app', cookie: { maxAge: 1 * 1000 * 60 * 60 * 24 * 365
 require("dotenv").config();
 const keys = require("./keys.js");
 var connection = mysql.createConnection(keys.data);
+console.log(keys.data)
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
 
 // Creates the connection with the server and loads the product data upon a successful connection
-connection.connect(function (err) {
+connection.connect(function(err) {
     if (err) {
         console.log(err);
     }
     console.log("Database connected");
 });
-app.get("/index", function (req, res) {
+app.get("/index", function(req, res) {
     res.render("index");
 });
 
-app.get("/OBD_LookUp", function (req, res) {
+app.get("/OBD_LookUp", function(req, res) {
     res.render("obdlookup");
 });
 
-app.get("/sign-up", function (req, res) {
+app.get("/sign-up", function(req, res) {
     res.render("signup");
 })
 
-app.get("/log-in", function (req, res) {
+app.get("/log-in", function(req, res) {
     res.render("login");
 });
 
-app.get("/vindecoder", function (req, res) {
+app.get("/vindecoder", function(req, res) {
     res.render("vindecoder");
 })
-app.post("/login", function (req, res) {
+app.post("/login", function(req, res) {
     var userEmail = req.body.email;
     var password = req.body.password;
 
-    connection.query('SELECT * FROM users LEFT JOIN car_data USING (user_id) WHERE user_email = ?', [userEmail], function (error, results, fields) {
+    connection.query('SELECT * FROM users LEFT JOIN car_data USING (user_id) WHERE user_email = ?', [userEmail], function(error, results, fields) {
         if (error) throw error;
         if (results == 0) {
             res.send("Invalid Email and/or password. Please try again");
         } else {
-            bcrypt.compare(password, results[0].user_p_hash, function (err, result) {
+            bcrypt.compare(password, results[0].user_p_hash, function(err, result) {
 
                 if (result == true) {
 
@@ -94,7 +95,7 @@ app.post("/login", function (req, res) {
     });
 });
 
-app.get('/another-page', function (req, res) {
+app.get('/another-page', function(req, res) {
     var user_info = {
         user_id: req.session.user_id,
         email: req.session.email,
@@ -106,21 +107,21 @@ app.get('/another-page', function (req, res) {
     res.json(user_info);
 });
 
-app.get('/logout', function (req, res) {
-    req.session.destroy(function (err) {
+app.get('/logout', function(req, res) {
+    req.session.destroy(function(err) {
         res.send('you are logged out');
     })
 });
 
-app.get("/", function (req, res) {
-    connection.query("SELECT * FROM role_types", function (err, result) {
+app.get("/", function(req, res) {
+    connection.query("SELECT * FROM role_types", function(err, result) {
         res.render("index", { res: result });
     })
 })
 
 
 
-app.post("/signup", function (req, res) {
+app.post("/signup", function(req, res) {
     var userEmail = req.body.email;
     var userName = req.body.name;
     var password = req.body.password;
@@ -129,14 +130,14 @@ app.post("/signup", function (req, res) {
     var carYear = req.body.year;
 
     function selectNewUserId(email) {
-        connection.query("SELECT * FROM users WHERE user_email=?", [email], function (err, result) {
+        connection.query("SELECT * FROM users WHERE user_email=?", [email], function(err, result) {
             insertCar(result[0].user_id, carMake, carModel, carYear);
         })
     }
 
 
     function insertCar(userId, make, model, year) {
-        connection.query("INSERT INTO car_data (user_id, car_model, car_make, car_year) VALUES (?, ?, ?, ?)", [userId, make, model, year], function (err, result) {
+        connection.query("INSERT INTO car_data (user_id, car_model, car_make, car_year) VALUES (?, ?, ?, ?)", [userId, make, model, year], function(err, result) {
 
             res.render("profile", {
                 email: req.session.email,
@@ -148,9 +149,9 @@ app.post("/signup", function (req, res) {
         })
     }
 
-    bcrypt.genSalt(10, function (err, salt) {
-        bcrypt.hash(password, salt, function (err, p_hash) {
-            connection.query("INSERT INTO users(user_email, user_name, user_p_hash) VALUES (?, ?, ?)", [userEmail, userName, p_hash], function (err, result) {
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(password, salt, function(err, p_hash) {
+            connection.query("INSERT INTO users(user_email, user_name, user_p_hash) VALUES (?, ?, ?)", [userEmail, userName, p_hash], function(err, result) {
                 if (err) {
                     res.send("You need to use a unique email")
                 } else {
@@ -161,6 +162,6 @@ app.post("/signup", function (req, res) {
     });
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
     console.log("Listening on 3000");
 });
