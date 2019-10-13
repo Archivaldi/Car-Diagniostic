bf83af534dc6e321679ef83b81873f55b249d5f8
Irina K <irtiola@gmail.com>
2019-10-13 08:04:20 -0700

    new styling+modal

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+
 var bodyParser = require("body-parser");
 app.set("view engine", "ejs");
 
@@ -18,6 +19,10 @@ app.use(cookieParser());
 //allow sessions
 app.use(session({ secret: 'app', cookie: { maxAge: 1 * 1000 * 60 * 60 * 24 * 365 } }));
 
+app.use(function (req, res, next) {
+    res.locals.user = req.session.name || '';
+    next();
+});
 
 //add dotenv package for hiding private data
 require("dotenv").config();
@@ -34,6 +39,7 @@ connection.connect(function (err) {
     }
     console.log("Database connected");
 });
+
 app.get("/index", function (req, res) {
     res.render("index");
 });
@@ -51,8 +57,17 @@ app.get("/log-in", function (req, res) {
 });
 
 app.get("/vindecoder", function (req, res) {
-    res.render("vindecoder");
-})
+    res.render("vindecoder", { car_vin: res.locals.car_vin });
+});
+
+app.get("/profile", function (req, res) {
+    res.render("profile", {
+        email: req.session.email,
+        name: req.session.name,
+        cars: req.session.user_cars
+    });
+});
+
 app.post("/login", function (req, res) {
     var userEmail = req.body.email;
     var password = req.body.password;
@@ -65,50 +80,45 @@ app.post("/login", function (req, res) {
             bcrypt.compare(password, results[0].user_p_hash, function (err, result) {
 
                 if (result == true) {
-
+                    req.session.logged_in = true;
                     req.session.user_id = results[0].user_id;
                     req.session.email = results[0].user_email;
                     req.session.name = results[0].user_name;
-                    var usersCars = [];
+                    res.locals.user = req.session.name;
+                    var userCars = req.session.user_cars = [];
+
                     for (var i = 0; i < results.length; i++) {
                         var car = {};
                         car.car_model = results[i].car_model;
                         car.car_make = results[i].car_make;
                         car.car_year = results[i].car_year;
-                        usersCars.push(car);
+                        car.car_vin = results[i].car_vin;
+                        userCars.push(car);
                     }
-                    req.session.user_cars = usersCars;
 
-                    res.render("profile", {
-                        email: req.session.email,
-                        name: req.session.name,
-                        cars: req.session.user_cars
-                    });
 
+                    res.redirect("/profile");
 
                 } else {
                     res.redirect('/');
-                }
+                };
             });
-        }
+        };
     });
 });
-
 app.get('/another-page', function (req, res) {
     var user_info = {
         user_id: req.session.user_id,
         email: req.session.email,
         name: req.session.name,
-        car_model: req.session.car_model,
-        car_make: req.session.car_make,
-        car_year: req.session.car_year
+        cars: req.session.user_cars
     }
     res.json(user_info);
 });
 
 app.get('/logout', function (req, res) {
     req.session.destroy(function (err) {
-        res.send('you are logged out');
+        res.redirect('/');
     })
 });
 
@@ -127,19 +137,20 @@ app.post("/signup", function (req, res) {
     var carMake = req.body.carMake;
     var carModel = req.body.carModel;
     var carYear = req.body.year;
-    var carVin = req.body.carVin
+    var carVin = req.body.carVin;
+
 
     function selectNewUserId(email) {
-        connection.query("SELECT * FROM users WHERE user_email=?", [email], function(err, result) {
+        connection.query("SELECT * FROM users WHERE user_email=?", [email], function (err, result) {
             insertCar(result[0].user_id, carMake, carModel, carYear, carVin);
         })
     }
 
 
     function insertCar(userId, make, model, year, vin) {
-        connection.query("INSERT INTO car_data (user_id, car_model, car_make, car_year, car_vin) VALUES (?, ?, ?, ?, ?)", [userId, make, model, year, vin], function (err, result) {
+        connection.query("INSERT INTO car_data (user_id, car_make, car_model, car_year, car_vin) VALUES (?, ?, ?, ?, ?)", [userId, make, model, year, vin], function (err, result) {
             req.session.logged_in = true;
-            req.session.user_id = userId; 
+            req.session.user_id = userId;
 
             connection.query("SELECT * FROM users LEFT JOIN car_data USING (user_id) WHERE user_id = ?", [userId], function (err, results) {
 
@@ -149,6 +160,7 @@ app.post("/signup", function (req, res) {
                 req.session.car_model = results[0].car_model;
                 req.session.car_year = results[0].car_year;
                 req.session.car_vin = results[0].car_vin;
+                res.locals.user = req.session.name;
                 var car = {
                     car_make: req.session.car_make,
                     car_model: req.session.car_model,
@@ -166,18 +178,17 @@ app.post("/signup", function (req, res) {
     }
 
     bcrypt.genSalt(10, function (err, salt) {
-            bcrypt.hash(password, salt, function (err, p_hash) {
-                connection.query("INSERT INTO users(user_email, user_name, user_p_hash) VALUES (?, ?, ?)", [userEmail, userName, p_hash], function (err, result) {
-                    if (err) {
-                        res.send("You need to use a unique email")
-                    } else {
-                        selectNewUserId(userEmail);
-                    };
-                });
+        bcrypt.hash(password, salt, function (err, p_hash) {
+            connection.query("INSERT INTO users(user_email, user_name, user_p_hash) VALUES (?, ?, ?)", [userEmail, userName, p_hash], function (err, result) {
+                if (err) {
+                    res.send("You need to use a unique email")
+                } else {
+                    selectNewUserId(userEmail);
+                };
             });
-
         });
     });
+});
 
 app.listen(3000, function () {
     console.log("Listening on 3000");
